d38df1d4f871f7c1596ef18879ceaf56b0bebaa5
Shotaro Oyama <shotaro12oyama@gmail.com>
2018-05-11 07:13:15 +0900

    minor upadte

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -10,17 +10,6 @@ from cv_bridge import CvBridge
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
-
-class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
-
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
-
 from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
