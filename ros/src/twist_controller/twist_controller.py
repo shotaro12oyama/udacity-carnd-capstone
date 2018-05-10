8b0dae9d1ebea34cc02c2e9482abcf6080125637
shotaro12oyama <shotaro12oyama@gmail.com>
2018-05-10 15:31:58 +0900

    After looking through walkthrough video

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,156 @@
+import rospy
+from lowpass import LowPassFilter
+from pid import PID
+from yaw_controller import YawController
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self,
+                 vehicle_mass, fuel_capacity,
+                 brake_deadband, decel_limit, accel_limit, wheel_radius,
+                 wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
+        
+        self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+        kp = 0.3
+        ki = 0.1
+        kd = 0.
+        mn = 0.   # max throttle
+        mx = 0.2  # min throttle
+        self.throttle_controller = PID(kp, ki, kd, mn, mx)
+
+        tau = 0.5 # 1/(2pi*tau) = cutoff frequency
+        ts = 0.02 # Sample time
+        self.vel_lpf = LowPassFilter(tau, ts)
+#!/usr/bin/env python
+
+import os
+import csv
+import math
+
+from geometry_msgs.msg import Quaternion
+
+from styx_msgs.msg import Lane, Waypoint
+
+import tf
+import rospy
+
+CSV_HEADER = ['x', 'y', 'z', 'yaw']
+MAX_DECEL = 1.0
+
+
+class WaypointLoader(object):
+
+    def __init__(self):
+        rospy.init_node('waypoint_loader', log_level=rospy.DEBUG)
+
+        self.pub = rospy.Publisher('/base_waypoints', Lane, queue_size=1, latch=True)
+
+        self.velocity = self.kmph2mps(rospy.get_param('~velocity'))
+        self.new_waypoint_loader(rospy.get_param('~path'))
+        rospy.spin()
+
+    def new_waypoint_loader(self, path):
+        if os.path.isfile(path):
+            waypoints = self.load_waypoints(path)
+            self.publish(waypoints)
+            rospy.loginfo('Waypoint Loded')
+        else:
+            rospy.logerr('%s is not a file', path)
+
+    def quaternion_from_yaw(self, yaw):
+        return tf.transformations.quaternion_from_euler(0., 0., yaw)
+
+    def kmph2mps(self, velocity_kmph):
+        return (velocity_kmph * 1000.) / (60. * 60.)
+
+    def load_waypoints(self, fname):
+        waypoints = []
+        with open(fname) as wfile:
+            reader = csv.DictReader(wfile, CSV_HEADER)
+            for wp in reader:
+                p = Waypoint()
+                p.pose.pose.position.x = float(wp['x'])
+                p.pose.pose.position.y = float(wp['y'])
+                p.pose.pose.position.z = float(wp['z'])
+                q = self.quaternion_from_yaw(float(wp['yaw']))
+                p.pose.pose.orientation = Quaternion(*q)
+                p.twist.twist.linear.x = float(self.velocity)
+
+                waypoints.append(p)
+        return self.decelerate(waypoints)
+
+    def distance(self, p1, p2):
+        x, y, z = p1.x - p2.x, p1.y - p2.y, p1.z - p2.z
+        return math.sqrt(x*x + y*y + z*z)
+
+    def decelerate(self, waypoints):
+        last = waypoints[-1]
+        last.twist.twist.linear.x = 0.
+        for wp in waypoints[:-1][::-1]:
+            dist = self.distance(wp.pose.pose.position, last.pose.pose.position)
+            vel = math.sqrt(2 * MAX_DECEL * dist)
+            if vel < 1.:
+                vel = 0.
+            wp.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
+        return waypoints
+
+    def publish(self, waypoints):
+        lane = Lane()
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time(0)
+        lane.waypoints = waypoints
+        self.pub.publish(lane)
+
+
+if __name__ == '__main__':
+    try:
+        WaypointLoader()
+    except rospy.ROSInterruptException:
+        rospy.logerr('Could not start waypoint node.')
+
+        self.vehicle_mass = vehicle_mass
+        self.fuel_capacity = fuel_capacity
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
+
+        self.last_time = rospy.get_time()
+
+    def reset(self):
+        self.throttle_controller.reset()
+
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+        """Returns (throttle, brake, steer), or None"""
+
+        if not dbw_enabled:
+            self.reset()
+            return None
+
+        current_vel = self.vel_lpf.filt(current_vel)
+
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+
+        vel_error = linear_vel - current_vel
+
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0
+
+        if linear_vel == 0. and current_vel < 0.1:
+            throttle = 0
+            brake = 400 # N*m - to hold teh car in place if we are stopped at a light. Acceleration - 1m/s^2
+
+        elif throttle < .1 and vel_error < 0:
+            throttle = 0
+            decel = max(vel_error, self.decel_limit)
+            brake = abs(decel) * self.vehicle_mass * self.wheel_radius # Torque N*m
+
+        return throttle, brake, steering
